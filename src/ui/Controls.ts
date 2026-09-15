d7cc6e90b43fe17422c3974e660c59e5090c1954
Raymond L Maple <rmaple@neumont.edu>
2026-09-14 18:18:59 -0600

    style: use looking-glass-icon.png for topbar title logo

--- src/ui/Controls.ts
+++ src/ui/Controls.ts
@@ -173,7 +173,7 @@ export class Controls {
     this.topBar.innerHTML = `
       <div class="topbar-left">
         <div class="topbar-brand">
-          <img src="/looking-glass-logo.png" alt="Looking Glass Logo" class="app-logo" />
+          <img src="/looking-glass-icon.png" alt="Looking Glass" class="app-logo" />
           <span class="app-title">Looking Glass</span>
         </div>
         <div class="debug-hud-box" id="debug-hud-box">
